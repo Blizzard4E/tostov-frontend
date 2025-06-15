af1e17f4d88e87517cf5425f34a55e803bf18cf6
Blizzard4E <blizzard4e@gmail.com>
2025-06-15 10:21:44 +0700

    add google verify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,35 +1,45 @@
 import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-																																compatibilityDate: "2025-05-15",
-																																devtools: { enabled: true },
-																																css: ["~/assets/css/tailwind.css"],
+	app: {
+		head: {
+			meta: [
+				{
+					name: "google-site-verification",
+					content: "vq9HEZ5Axcng5IeqILdSJq6t9UEs1Ze1M_TvFY3UpKg",
+				},
+			],
+		},
+	},
+	compatibilityDate: "2025-05-15",
+	devtools: { enabled: true },
+	css: ["~/assets/css/tailwind.css"],
 
-																																vite: {
-																																																																plugins: [tailwindcss()],
-																																},
+	vite: {
+		plugins: [tailwindcss()],
+	},
 
-																																modules: ["shadcn-nuxt", "@nuxtjs/supabase", "@nuxt/fonts", "@nuxt/icon"],
-																																shadcn: {
-																																																																/**
-																																																																								* Prefix for all the imported component
-																																																																								*/
-																																																																prefix: "",
-																																																																/**
-																																																																								* Directory that the component lives in.
-																																																																								* @default "./components/ui"
-																																																																								*/
-																																																																componentDir: "./components/ui",
-																																},
-																																supabase: {
-																																																																redirect: false,
-																																},
-																																runtimeConfig: {
-																																																																public: {
-																																																																																																cloud_name: process.env.PUBLIC_CLOUD_NAME,
-																																																																																																cloud_key: process.env.PUBLIC_CLOUD_API_KEY,
-																																																																																																preset_name: process.env.PUBLIC_PRESET_NAME,
-																																																																},
-																																},
-																																ssr: false,
+	modules: ["shadcn-nuxt", "@nuxtjs/supabase", "@nuxt/fonts", "@nuxt/icon"],
+	shadcn: {
+		/**
+		 * Prefix for all the imported component
+		 */
+		prefix: "",
+		/**
+		 * Directory that the component lives in.
+		 * @default "./components/ui"
+		 */
+		componentDir: "./components/ui",
+	},
+	supabase: {
+		redirect: false,
+	},
+	runtimeConfig: {
+		public: {
+			cloud_name: process.env.PUBLIC_CLOUD_NAME,
+			cloud_key: process.env.PUBLIC_CLOUD_API_KEY,
+			preset_name: process.env.PUBLIC_PRESET_NAME,
+		},
+	},
+	ssr: false,
 });
